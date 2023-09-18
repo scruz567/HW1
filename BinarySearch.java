90b2efb853098f8f643f36a84c75bc2359c8272b
scruz567 <120342729+scruz567@users.noreply.github.com>
2023-09-18 19:39:09 -0400

    Add files via upload

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,18 @@
 public class BinarySearch {
     public static int binarySearch(int[] sortedArray, int low, int high, int element) {
+        //edge cases
+        //Empty array case
+        if (sortedArray.length == 0) {
+            return -1;
+        }
+        //lower than lowest or higher than highest value in the array edge case
+        if (element < sortedArray[low]) {
+            return -1;
+        }
+        if (element > sortedArray[high]) {
+            return -1;
+        }
+
         if (low > high) {
             return -1;
         }
@@ -15,7 +28,7 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] sortedArray = new int[150000]; // change size of array
+        int[] sortedArray = new int[2000000]; // change size of array OR input custom elements
         int target = 7;
         long startTime = System.nanoTime(); // start runtime after array is created
         int index = binarySearch(sortedArray, 0, sortedArray.length - 1, target);
